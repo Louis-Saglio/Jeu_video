c08e9a10c8c2d8cc0b06d3b504d6aaae53e6067b
Louis-Saglio <louis.saglio@sfr.fr>
2017-04-10 09:42:28 +0200

    Nouvelle classe IA

--- dev.js
+++ dev.js
@@ -4,6 +4,28 @@
 
 window.onload = function () {
 
+    function hasard(min, max) {
+        var a = Math.random() * (max - min);
+        return Math.round(a + min);
+    }
+
+    function hasard_item(liste) {
+        return liste[hasard(0, liste.length)];
+    }
+
+    function hasard_pol(val) {
+        if (hasard(0, 1) === 0) {
+            return val * -1;
+        }
+        else {
+            return val;
+        }
+    }
+
+    function print(param) {
+        console.log(param);
+    }
+
     var canvas1 = document.getElementById("canvas");
     var canvas = canvas1.getContext("2d");
 
@@ -55,7 +77,7 @@ window.onload = function () {
         largeur_case : 50,
         largeur_plateau : 11,
         plateau : [],
-        chat : {x: 5, y: 5},
+        chat : {x: 7, y: 3},
 
         init : function () {
             for (var ligne=0; ligne<this.largeur_plateau; ligne+=1){
@@ -76,7 +98,7 @@ window.onload = function () {
         },
 
 
-        move_chat : function (dir) {
+        find_move_factor : function (dir) {
             var y = 1;
             if (dir[0] === "h") {
                 y = -1;
@@ -102,7 +124,13 @@ window.onload = function () {
                     x = 1;
                 }
             }
-            this.set_chat(this.chat.x + x, this.chat.y + y);
+            return {"x": x, "y": y};
+        },
+
+
+        move_chat : function (dir) {
+            var mv = this.find_move_factor(dir);
+            this.set_chat(this.chat.x + mv.x, this.chat.y + mv.y);
         },
 
         refresh : function () {
@@ -111,6 +139,7 @@ window.onload = function () {
                     this.plateau[ligne][colonne].show();
                 }
             }
+            this.set_chat(this.chat.x, this.chat.y);
         },
 
         onMouse_case : function (x, y) {
@@ -124,16 +153,100 @@ window.onload = function () {
 
     };
 
+
+    var Ia = {
+
+        refresh : function (plateau) {
+            this.plateau = plateau.plateau;
+            this.chat = plateau.chat;
+            this.direction = hasard_item("hbgd");
+        },
+
+        find_move_case : function (dir, chat) {
+            var y = 1;
+            if (dir[0] === "h") {
+                y = -1;
+            }
+            if (dir[1] === "g") {
+                var x = -1;
+                if (chat.y % 2 === 0) {
+                    x = 0;
+                }
+            }
+            if (dir[1] === "d") {
+                var x = 0;
+                if (chat.y % 2 === 0) {
+                    x = 1;
+                }
+            }
+            if (dir[0] === "m") {
+                y = 0;
+                if (dir[1] === "g") {
+                    x = -1;
+                }
+                if (dir[1] === "d") {
+                    x = 1;
+                }
+            }
+            return {"x": chat.x + x, "y": chat.y + y};
+        },
+
+        find_relative_case : function (dir, num, chat) {
+            for (var i=0; i<num; i++) {
+                chat = this.find_move_case(dir, chat);
+            }
+            return chat;
+        },
+
+        find_one_axe : function (dir, chat) {
+            var axe = [];
+            for (var i=0; i<11; i++){
+                axe.push(this.find_relative_case(dir, i, chat));
+            }
+            return axe;
+        },
+
+        find_axes : function (chat) {
+            var axes = {"hg": [], "hd": [], "bg": [], "bd": [], "mg": [], "md": []};
+            for (var dir in axes) {
+                axes[dir] = this.find_one_axe(dir, chat);
+            }
+            return axes;
+        },
+        
+        decide : function () {
+            if (this.direction === "h") {
+                var newChat = this.find_move_case("h" + hasard_item("gd"), this.chat);
+            }
+            if (this.direction === "b") {
+                var newChat = this.find_move_case("b" + hasard_item("gd"), this.chat);
+            }
+            if (this.direction === "g") {
+                var newChat = this.find_move_case(hasard_item("hb" + "g"), this.chat);
+            }
+            if (this.direction === "d") {
+                var newChat = this.find_move_case(hasard_item("hb") + "d", this.chat);
+            }
+
+        }
+    };
+
+
     var a = Object.create(Plateau);
     a.init();
+    var b = Object.create(Ia);
     a.refresh();
+    b.refresh(a);
 
     canvas1.onclick = function () {
         var X = event.clientX;
         var Y = event.clientY;
         a.onMouse_case(X, Y).set_apparence("rond2.jpg");
-        a.move_chat("mg");
+        //print(b.find_axes(b.chat));
+        b.decide();
         a.refresh();
+        b.refresh(a);
+
     }
 
 };
